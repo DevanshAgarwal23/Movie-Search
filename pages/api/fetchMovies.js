9617190778188e08564036514d06e0476bb96351
Devansh Agarwal <devanshagarwal@pop-os.localdomain>
2020-12-01 23:47:16 +0530

    changed hhtp to https

--- pages/api/fetchMovies.js
+++ pages/api/fetchMovies.js
@@ -5,7 +5,7 @@ async function fetchMovies(name,year) {
 
     try {
       
-      const res = await fetch(`http://www.omdbapi.com/?apikey=fccd414a&s=${name}&y=${year}`);
+      const res = await fetch(`https://www.omdbapi.com/?apikey=fccd414a&s=${name}&y=${year}`);
       const data = await res.json();
      
       return data;
